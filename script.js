e1c8366ee82ce526bfcfff0bd1fa78c56fd6199c
clia7 <93claudia7@gmail.com>
2025-05-08 19:07:22 +0200

    added some emission data, changed searching function, some css changes

--- script.js
+++ script.js
@@ -22,20 +22,24 @@ function showSidebar() {
 
   //FILTER FOR TABLE
   function filterfunction() {
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, table, tr, td, i, j, txtValue, rowMatches;
     input = document.getElementById("filterInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("emissionTable");
     tr = table.getElementsByTagName("tr");
+  
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
+      td = tr[i].getElementsByTagName("td");
+      rowMatches = false;
+  
+      for (j = 0; j < td.length; j++) {
+        txtValue = td[j].textContent || td[j].innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+          rowMatches = true;
+          break;
         }
-      }       
+      }
+  
+      tr[i].style.display = rowMatches ? "" : "none";
     }
   }
